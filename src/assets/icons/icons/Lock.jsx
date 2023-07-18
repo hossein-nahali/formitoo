95bca9dc4bb6a0a8618c5294fdc63e1f41c7775b
hossein-nahali <82251151+hossein-nahali@users.noreply.github.com>
2023-07-18 15:08:27 +0330

    add custom color in all icon

--- src/assets/icons/icons/Lock.jsx
+++ src/assets/icons/icons/Lock.jsx
@@ -1,18 +1,23 @@
-export const Lock = () => (
+export const Lock = ({color}) => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="vuesax/linear/lock">
             <g id="vuesax/linear/lock_2">
                 <g id="lock">
-                    <path id="Vector" d="M6 10V8C6 4.69 7 2 12 2C17 2 18 4.69 18 8V10" stroke="#292D32"
+                    <path id="Vector" d="M6 10V8C6 4.69 7 2 12 2C17 2 18 4.69 18 8V10"
+                          stroke={color ? color : "#292D32"}
                           strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                     <path id="Vector_2"
                           d="M17 22H7C3 22 2 21 2 17V15C2 11 3 10 7 10H17C21 10 22 11 22 15V17C22 21 21 22 17 22Z"
-                          stroke="#292D32" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    <path id="Vector_3" d="M15.9965 16H16.0054" stroke="#292D32" strokeWidth="2" strokeLinecap="round"
+                          stroke={color ? color : "#292D32"} strokeWidth="1.5" strokeLinecap="round"
                           strokeLinejoin="round"/>
-                    <path id="Vector_4" d="M11.9955 16H12.0045" stroke="#292D32" strokeWidth="2" strokeLinecap="round"
+                    <path id="Vector_3" d="M15.9965 16H16.0054" stroke={color ? color : "#292D32"} strokeWidth="2"
+                          strokeLinecap="round"
                           strokeLinejoin="round"/>
-                    <path id="Vector_5" d="M7.99451 16H8.00349" stroke="#292D32" strokeWidth="2" strokeLinecap="round"
+                    <path id="Vector_4" d="M11.9955 16H12.0045" stroke={color ? color : "#292D32"} strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"/>
+                    <path id="Vector_5" d="M7.99451 16H8.00349" stroke={color ? color : "#292D32"} strokeWidth="2"
+                          strokeLinecap="round"
                           strokeLinejoin="round"/>
                 </g>
             </g>
