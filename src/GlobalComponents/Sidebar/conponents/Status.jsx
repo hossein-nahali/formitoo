6a0ba4c36a1d08c04e039384b2429fb024c4200f
hossein-nahali <82251151+hossein-nahali@users.noreply.github.com>
2023-07-23 17:14:17 +0330

    remove  react-custom-scrollbars-2 in src

--- src/GlobalComponents/Sidebar/conponents/Status.jsx
+++ src/GlobalComponents/Sidebar/conponents/Status.jsx
@@ -1,7 +1,6 @@
 import {Language} from "../../../config/Language.js";
 import {Setting} from "../../../assets/icons/index.js";
 import {StatusNews} from "../../../config/fakeData/StatusNews.js";
-import {Scrollbars} from 'react-custom-scrollbars-2';
 
 export const Status = () => {
     const {status} = Language.fa
@@ -9,18 +8,14 @@ export const Status = () => {
     return (
         <div className="status scroll-rtl">
             <h3><Setting/>{status}</h3>
-            <Scrollbars
-                style={{height: 150, padding: '15px 0'}}
-                renderTrackHorizontal={props => <div {...props} style={{display: 'none'}}/>}>
-                {StatusNews.map(news => {
-                    return (
-                        <div className={'news'} key={news.id}>
-                            <h4>{news.title}</h4>
-                            <p>{news.content}</p>
-                        </div>
-                    )
-                })}
-            </Scrollbars>
+            {StatusNews.map(news => {
+                return (
+                    <div className={'news'} key={news.id}>
+                        <h4>{news.title}</h4>
+                        <p>{news.content}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
--- src/GlobalComponents/Sidebar/index.jsx
+++ src/GlobalComponents/Sidebar/index.jsx
@@ -1,19 +1,27 @@
 import './assets/css/Sidebar.scss'
 import {Profile, Search, SliderAuthor, Status, TotalParticipant} from "./conponents";
-import {Scrollbars} from 'react-custom-scrollbars-2';
+import {Language} from "../../config/Language.js";
+import {useContext} from "react";
+import GeneralContext from "../../context/GeneralContext.js";
 
 
-export const Sidebar = () => {
+export const Sidebar = (props) => {
+    const {setting} = Language.fa
+    const {state, dispatch} = useContext(GeneralContext)
 
     return (
-        <div className={'sidebar-user scroll-rtl'}>
-            <Scrollbars renderTrackHorizontal={props => <div {...props} style={{display: 'none'}}/>}>
-                <Profile/>
-                <Search/>
-                <TotalParticipant/>
-                <SliderAuthor/>
-                <Status/>
-            </Scrollbars>
+        <div className={`sidebar-user ${props?.className ? props?.className : ''}`}>
+            <div className={'title-sidebar'}>
+                <h2>{setting}</h2>
+                <div className={'close'}>
+                    <i className="gg-close" onClick={() => dispatch({type: 'close_sidebar'})}/>
+                </div>
+            </div>
+            <Profile/>
+            <Search/>
+            <TotalParticipant/>
+            <SliderAuthor/>
+            <Status/>
         </div>
     )
 }
