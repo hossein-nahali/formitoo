46300840c1bb8355d763b432780b4b0f6820be97
hossein-nahali <82251151+hossein-nahali@users.noreply.github.com>
2023-07-23 17:52:46 +0330

    Update ArrowLeft.jsx

--- src/assets/icons/icons/ArrowLeft.jsx
+++ src/assets/icons/icons/ArrowLeft.jsx
@@ -1,5 +1,5 @@
 export const ArrowLeft = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
         <path d="M15 19.9201L8.47997 13.4001C7.70997 12.6301 7.70997 11.3701 8.47997 10.6001L15 4.08008"
               stroke={props.color ? props.color : "#292D32"} strokeWidth="1.5" strokeMiterlimit="10"
               strokeLinecap="round"
