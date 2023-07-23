eb68c391ee66b8f94796da75d1d1a065836f2b3f
hossein-nahali <82251151+hossein-nahali@users.noreply.github.com>
2023-07-23 17:15:33 +0330

    Update GeneralStatus.jsx

--- src/pages/Home/Components/GeneralStatus.jsx
+++ src/pages/Home/Components/GeneralStatus.jsx
@@ -6,7 +6,24 @@ export const GeneralStatus = () => {
 
     return (
         <div className={'general-status'}>
-            <Swiper slidesPerView={4} spaceBetween={15} loop={true}>
+            <Swiper slidesPerView={4} spaceBetween={15} loop={true} breakpoints={{
+                320: {
+                    // width: 576,
+                    slidesPerView: 1.5,
+                },
+                768: {
+                    slidesPerView: 2
+                },
+                1024: {
+                    spaceBetween: 10,
+                    slidesPerView: 3
+
+                },
+                1440: {
+                    slidesPerView: 4
+
+                }
+            }}>
                 {GeneralStatusInfo.map(box => {
                     return (
                         <SwiperSlide key={box.id}>
