2582b37684c52f7943ebbc378ec25bdfc7322f47
hossein-nahali <82251151+hossein-nahali@users.noreply.github.com>
2023-07-27 13:34:56 +0330

    complete option table

--- src/GlobalComponents/Option/index.jsx
+++ src/GlobalComponents/Option/index.jsx
@@ -1,4 +1,27 @@
-export const Option = () => {
+import {Copy, Eye as EyeIcon, GridEdit, Trash} from './../../assets/icons'
 
+export const Option = (props) => {
+    const {
+        icons = [],
+    } = props
 
+    const IconGenerator = () => {
+        return icons.map(icon => {
+            switch (icon.type) {
+                case "eye":
+                    return <EyeIcon key={icon.type}/>
+                case "edit":
+                    return <GridEdit key={icon.type}/>
+                case "trash":
+                    return <Trash key={icon.type}/>
+                case "copy":
+                    return <Copy key={icon.type}/>
+
+                default:
+                    return <p>not found icon</p>
+            }
+        })
+    }
+
+    return <div className={'option-table'}><IconGenerator/></div>
 }
