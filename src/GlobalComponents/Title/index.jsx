dd769ae4209e8a02dd2ee408ad0fe4ade1837d02
hossein-nahali <82251151+hossein-nahali@users.noreply.github.com>
2023-07-29 18:29:33 +0330

    Update index.jsx

--- src/GlobalComponents/Title/index.jsx
+++ src/GlobalComponents/Title/index.jsx
@@ -14,7 +14,7 @@ export const Title = ({title, is_show_desc, presonal, desc}) => {
                 <h1>{title}</h1>
                 {
                     is_show_desc ?
-                        desc ? desc :
+                        desc ? <p>{desc}</p> :
                             <p>{status_desc_title.today}
                                 <span>{presonal ? presonal : 27}</span> {status_desc_title.person} {status_desc_title.in_test}
                                 <span>{`${personality_test} `} </span> {status_desc_title.participated}</p>
